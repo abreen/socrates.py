1b78017fdcc77fa41ee5f8786a521e7fe3d72501
Alexander Breen <alexander.breen@gmail.com>
2014-10-02 18:55:43 -0400

    "Baby's first commit" needed a little love
    I'm just joshin', Chris.

--- grader.py
+++ grader.py
@@ -22,52 +22,71 @@ def grade(criteria, submissions, filename):
                 found.append(f)
                 break
         else:
-            #Informing grader, gathering input filename
+            # in the case that the file is missing, we should prompt
+            # the grader to pick the incorrect file name or declare
+            # the file missing
+
             sprint(COLOR_YELLOW + "could not find "
                    "file '{}'".format(f.path) + COLOR_RESET)
 
             if len(submissions) < 1:
                 continue
 
-            cur_dir = os.path.abspath(os.path.split(submissions[0])[0])            
-            sprint(COLOR_YELLOW + "listing submissions..." + COLOR_RESET)
-            sprint(COLOR_GREEN + " ".join(submissions) + COLOR_RESET)
-            sname = input(COLOR_CYAN + "Enter a file to grade, 0 to declare file missing, -1 to skip this submission: " + COLOR_RESET)
+            # find the submission directory (it could be the
+            # current working directory, but maybe not)
+            submission_dir, _ = os.path.split(submissions[0])
+
+            if not submission_dir:
+                submission_dir = os.path.abspath(os.curdir)
+
+            sprint("files in submission directory:")
+            for s in submissions:
+                print(s)
+
+            sprint(COLOR_CYAN + "did the student name the file "
+                   "incorrectly?" + COLOR_RESET)
+            sname = input(COLOR_CYAN + "enter a file name, 0 to "
+                          "declare file missing, or -1 to skip this "
+                          "submission: " + COLOR_RESET)
 
             if sname == '0':
                 num_missing += 1
                 continue
+
             elif sname == '-1':
-                sprint("Deferring submission.")
+                sprint("deferring submission; no grade file written")
                 sys.exit(EXIT_WITH_DEFER)
+
             else:
-                #get absolute path to the old and new files
-                opath = os.path.join(cur_dir,sname)
-                npath = os.path.join(cur_dir,crit_name)
-                
+                # get absolute path to the old and new files
+                opath = os.path.join(submission_dir, sname)
+                npath = os.path.join(submission_dir, crit_name)
+
                 try:
-                    ofile = open(opath,'r')
-                    nfile = open(npath,'w')
+                    ofile = open(opath, 'r')
+                    nfile = open(npath, 'w')
                 except FileNotFoundError:
-                    sprint("Could not open the specified file", error=True)
+                    sprint("could not open the correct file", error=True)
                     sys.exit(ERR_GRADING_MISC)
 
-                try:    
-                    shutil.copyfileobj(ofile,nfile)
-                except (IOerror,os.error):
-                    sprint("Could not copy the data to the new file", error=True)
+                try:
+                    shutil.copyfileobj(ofile, nfile)
+                except:
+                    sprint("error copying to correct file", error=True)
                     sys.exit(ERR_GRADING_MISC)
-                
+
                 ofile.close()
                 nfile.close()
-                
-                #copy file stat for use in late penalty checking
+
+                # to preserve the modification time of the submission
+                # and not mess up late penalty checking, we'll copy
+                # the file data into the new file
                 try:
-                    shutil.copystat(opath,npath)
-                except os.error:
-                    sprint("Could not copy the file stat to the new file", error=True)
+                    shutil.copystat(opath, npath)
+                except:
+                    sprint("error copying stat to correct file", error=True)
                     sys.exit(ERR_GRADING_MISC)
-                    
+
                 found.append(f)
 
     out = io.StringIO()
