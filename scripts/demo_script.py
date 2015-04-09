84464ea037a5732bf28ae0b1fad041d71ad6cecf
Alexander Breen <abreen@bu.edu>
2015-04-08 23:58:19 -0400

    Small fixes

--- scripts/demo_script.py
+++ scripts/demo_script.py
@@ -8,12 +8,12 @@ from os import listdir
 # use the module's contents as if a simple "import <module>"
 # was already done
 
-# after this script runs, socrates will inspect this varialbe
+# after this script runs, socrates will inspect this variable
 # for a deduction dictionary
 _socrates_result = None
 
 print("Hello, world!")
-print(listdir('.'))
+print(str(listdir('.')))
 
 if False:
     # do the following to tell socrates to take a deduction
